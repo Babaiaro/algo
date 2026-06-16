12512e75e1069ca88b0433eb15d70a0e33eb9443
Babaiar <119310971+Babaiaro@users.noreply.github.com>
2026-06-15 23:57:53 -0500

    Add files via upload

---
+++ attributes.java
@@ -0,0 +1,12 @@
+package jun.oop;
+
+public class attributes {
+    int x = 5;
+
+    public static void main(String[] args){
+        attributes myObj = new attributes();
+        myObj.x = 25;
+        System.out.println(myObj.x);
+    }
+    
+}
