785de383a2691dca330bee1d3b3be0203967e341
Babaiar <119310971+Babaiaro@users.noreply.github.com>
2026-06-24 06:33:24 -0500

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,24 @@
+package oop;
+
+interface Animal {
+    public void animalSound();
+    public void sleep();
+    
+}
+
+class Dog implements Animal{
+    public void animalSound() {
+        System.out.println("gaf gaf");
+    }
+    public void sleep() {
+        System.out.println("ZZzzz");
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.animalSound();
+        myDog.sleep();
+    }
+}
